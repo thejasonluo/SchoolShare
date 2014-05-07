cc3896c7438209cda42a8d127743a0b3dbc20bd0
dsrein <dsrein@me.com>
2014-05-07 12:03:14 -0400

    doc stuff

--- app.py
+++ app.py
@@ -19,44 +19,44 @@ def home():
 
 @app.route("/register", methods = ["GET", "POST"])
 def register():
-    if "username" in session:
-    	return redirect(url_for("/"))
-    if request.method == "GET":
-        return render_template("register.html")
-    if "username" not in session:
-        username = request.form["username"]
-        password = request.form["password"]
-        confirm = request.form["confirm"]
-        securityq = request.form["securityq"]
-        securitya = request.form["securitya"]
-        if utils.register(username, password, confirm, securityq, securitya):
-            session["username"] = username
-            return redirect(url_for("search"))
+        if "username" in session:
+                return redirect(url_for("/"))
+                if request.method == "GET":
+                        return render_template("register.html")
+                        if "username" not in session:
+                                username = request.form["username"]
+                                password = request.form["password"]
+                                confirm = request.form["confirm"]
+                                securityq = request.form["securityq"]
+                                securitya = request.form["securitya"]
+                                if utils.register(username, password, confirm, securityq, securitya):
+                                        session["username"] = username
+                                        return redirect(url_for("search"))
+                                else:
+                                        return redirect(url_for("register"))
         else:
-             return redirect(url_for("register"))
-    else:
-         return redirect(url_for("search"))
+                return redirect(url_for("search"))
 
         
 @app.route("/login", methods = ["GET", "POST"])
 def login():
-    if "username" in session:
-    	return redirect(url_for("home.html"))
-    if request.method == "GET":
-        return render_template("index.html")
-    else:
-        username = request.form["username"]
-        password = request.form["password"]
-        if utils.authorize(username, password):
-            session["username"] = username
-            return redirect(url_for("search"))
-        else:
-            return redirect(url_for("login"))
+        if "username" in session:
+                return redirect(url_for("home.html"))
+                if request.method == "GET":
+                        return render_template("index.html")
+                else:
+                        username = request.form["username"]
+                        password = request.form["password"]
+                        if utils.authorize(username, password):
+                                session["username"] = username
+                                return redirect(url_for("search"))
+                        else:
+                                return redirect(url_for("login"))
 
 @app.route("/logout")
 def logout():
-    session.pop("username",None)
-    return redirect("/")
+        session.pop("username",None)
+        return redirect("/")
 
         
 
@@ -65,9 +65,9 @@ def logout():
 
 @app.route("/profile")
 def profile():
-    events = utils.getEvents(session["username"])
-    return render_template("profile.html", username = session['username'], events = events)
-    
+        events = utils.getEvents(session["username"])
+        return render_template("profile.html", username = session['username'], events = events)
+        
     
 @app.route("/addschool")
 def addschool():
@@ -78,7 +78,7 @@ def addschool():
                 else:
                         utils.addschool(session["username"], schoolname)
                         return render_template("school.html")
-                
+                        
         else:
                 return redirect("/login")
                 
@@ -94,7 +94,7 @@ def addclass():
                         return render_template("class.html")
         else:
                 return redirect("/login")
-    
+                        
 @app.route("/<Schoolname>")
 def school():
         return render_template("school.html")
@@ -109,29 +109,27 @@ def classname():
 
 @app.route("/<Schoolname>/<Subject>/<Classname>/addDoc")
 def addDoc():  
-    for f in request.files:
-        file = request.files[f]
-        qname=f
-        filename=file.filename
-        if (files[filename] == null):
-                file.save(filename)
-        else: 
-                render_template("docerror.html")       
-@app.route("/<Schoolname>/<Subject>/<Classname>/editDoc")
-def editDoc():
         for f in request.files:
-        file = request.files[f]
-        qname=f
-        filename=file.filename
-        if (files[filename] == null):
-               render_template("docerror2.html")
-        else: 
-                file.save(filename)
+                file = request.files[f]
+                filename=files.filename
+                if (files[filename] == null):
+                        file.save(filename)
+                else: 
+                        render_template("docerror.html")       
+                        @app.route("/<Schoolname>/<Subject>/<Classname>/editDoc")
+                        def editDoc():
+                                for f in request.files:
+                                        file = request.files[f]
+                                        filename=file.filename
+                                        if (files[filename] == null):
+                                                render_template("docerror2.html")
+                                        else: 
+                                                file.save(filename)
 
 
 
 
 if __name__=="__main__":
-    app.debug=True
-    app.run(host='0.0.0.0',port=5000)
+        app.debug=True
+        app.run(host='0.0.0.0',port=5000)
 

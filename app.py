ea38a5140c4ab3c9c12b7a24cafcc7170148508c
dsrein <dsrein@me.com>
2014-05-22 20:15:29 -0400

    fixing app

--- app.py
+++ app.py
@@ -21,14 +21,13 @@ def register():
         if request.method == "GET":
                 return render_template("register.html")
         elif "username" not in session:
-                username = request.form["username"]
-                password = request.form["password"]
-                confirm = request.form["confirm"]
                 firstname = request.form["first"]
                 lastname = request.form["securitya"]
-                school = request.form["school"]
+                username = request.form["username"]
+                password = request.form["password"]
+                email = request.form["email"]
                 if password == confirm:
-                        if utils.register(username, password, firstname, lastname, school):
+                        if utils.register(username, password, firstname, lastname, email):
                                 session["username"] = username
                                 return redirect(url_for("search"))
                         else:
